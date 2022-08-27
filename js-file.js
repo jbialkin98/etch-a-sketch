7851e952622c53dd0b570f3c95a5331c8c311231
Josh Bialkin <jbialkin98@gmail.com>
2022-08-27 16:23:47 -0400

    Add mouseover effect to boxes and fix grid
    Grid was not working with larger numbers because of added margins,
    so I changed the way the border is applied to the boxes.

--- js-file.js
+++ js-file.js
@@ -1,17 +1,27 @@
 const container = document.querySelector('.container');
 
-let blockInput = 16;
+let blockInput = 100;
+let blockSize = Math.floor((800 / blockInput) * 100) / 100;
 let blockNumber = Math.pow(blockInput, 2);
-let blockSize = 800 / blockInput;
 let blockSizeCSS = blockSize + 'px';
+console.log(blockSize);
 console.log(blockSizeCSS);
 
+// set up grid
 for (let i = 0; i < blockNumber; i++) {
     const block = document.createElement('div');
     block.classList.add('block');
     block.style.cssText = `width: ${blockSizeCSS}; height: ${blockSizeCSS}`;
+    if ((i+1) % blockInput == 0) {
+        block.style.borderRight= '1px solid';
+    }
     if (i >= blockNumber - blockInput) {
         block.style.borderBottom = '1px solid';
     }
     container.appendChild(block)
 }
+
+const blocks = document.querySelectorAll('.block');
+blocks.forEach(blocks => blocks.addEventListener('mouseover', function (e) {
+    e.target.style.background = 'gray';
+}));
