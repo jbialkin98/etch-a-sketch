2e16d082b5d09f3b49eff6ab67537f701c410101
Josh Bialkin <jbialkin98@gmail.com>
2022-08-27 15:02:24 -0400

    Make code for grid dynamic
    The code for the grid now works for any number of blocks, not
    just 8x8.

--- js-file.js
+++ js-file.js
@@ -1,13 +1,17 @@
 const container = document.querySelector('.container');
 
-let blockInput = 8;
+let blockInput = 16;
 let blockNumber = Math.pow(blockInput, 2);
+let blockSize = 800 / blockInput;
+let blockSizeCSS = blockSize + 'px';
+console.log(blockSizeCSS);
 
 for (let i = 0; i < blockNumber; i++) {
     const block = document.createElement('div');
     block.classList.add('block');
-    if (i >= blockNumber - 8) {
-        block.style.cssText = 'border-bottom: 1px solid';
+    block.style.cssText = `width: ${blockSizeCSS}; height: ${blockSizeCSS}`;
+    if (i >= blockNumber - blockInput) {
+        block.style.borderBottom = '1px solid';
     }
     container.appendChild(block)
 }
