f0da76c6ee35b79c903b100eb6904815dceda294
Josh Bialkin <jbialkin98@gmail.com>
2022-08-28 11:34:50 -0400

    Allow user to input desired width of grid
    Added a text box and a submit button for the user to type in a number
    and click submit. After clicking submit, the original grid is deleted
    and replaced by a new grid with the width/height the user inputted.
    
    Existing code was placed into functions: gridSetup(), sketching(), and
    deleteGrid().

--- js-file.js
+++ js-file.js
@@ -1,30 +1,47 @@
 const container = document.querySelector('.container');
 
-let blockInput = 100;
-let blockSize = Math.floor((800 / blockInput) * 100) / 100;
-let blockNumber = Math.pow(blockInput, 2);
-let blockSizeCSS = blockSize + 'px';
-console.log(blockSize);
-console.log(blockSizeCSS);
+let blockInput = 64;
+gridSetup(blockInput);
 
-// Set up grid
-for (let i = 0; i < blockNumber; i++) {
-    const block = document.createElement('div');
-    block.classList.add('block');
-    block.style.cssText = `width: ${blockSizeCSS}; height: ${blockSizeCSS}`;
+function gridSetup(blockInput) {
+    let blockNumber = Math.pow(blockInput, 2);
+    let blockSize = Math.floor((800 / blockInput) * 100) / 100;
+    let blockSizeCSS = blockSize + 'px';
 
-    // Add right border to last block of each row
-    if ((i+1) % blockInput == 0) {
-        block.style.borderRight= '1px solid';
+    for (let i = 0; i < blockNumber; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.cssText = `width: ${blockSizeCSS}; height: ${blockSizeCSS}`;
+
+        // Add right border to last block of each row
+        if ((i+1) % blockInput == 0) {
+            block.style.borderRight= '1px solid';
+        }
+        // Add bottom border to each block in the bottom row
+        if (i >= blockNumber - blockInput) {
+            block.style.borderBottom = '1px solid';
+        }
+        container.appendChild(block)
     }
-    // Add bottom border to each block in the bottom row
-    if (i >= blockNumber - blockInput) {
-        block.style.borderBottom = '1px solid';
-    }
-    container.appendChild(block)
+    sketching();
 }
 
-const blocks = document.querySelectorAll('.block');
-blocks.forEach(blocks => blocks.addEventListener('mouseover', function (e) {
-    e.target.style.background = 'gray';
-}));
+function sketching() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(blocks => blocks.addEventListener('mouseover', function (e) {
+        e.target.style.background = 'gray';
+    }));
+}
+
+let gridWidth = document.querySelector('#gridSize');
+const submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', function () {
+    let blockInput = document.getElementById('gridSize').value;
+    deleteGrid();
+    gridSetup(blockInput);
+});
+
+function deleteGrid() {
+    const blocks = document.querySelectorAll('.block');
+    blocks.forEach(blocks => blocks.remove());
+}
